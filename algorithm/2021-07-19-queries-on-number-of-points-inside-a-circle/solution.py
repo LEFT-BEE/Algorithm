386d62e7330aa99c7a89dbf385a69b78b2f889bb
한승진(20181512) <65720894+LEFT-BEE@users.noreply.github.com>
2021-07-19 13:49:13 +0900

    Create solution.py

---
+++ algorithm/2021-07-19-queries-on-number-of-points-inside-a-circle/solution.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/queries-on-number-of-points-inside-a-circle/submissions/
+class Solution:
+    def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
+        circle_map = []
+        for circle in queries:
+            circle_map.append(0)
+            for point in points:
+                if (circle[0]-point[0])*(circle[0]-point[0]) + \
+                (circle[1]-point[1])*(circle[1]-point[1]) <= circle[2]*circle[2]:
+                    circle_map[-1] += 1
+        return circle_map
